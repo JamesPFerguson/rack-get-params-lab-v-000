f07e909b5829af9dfba80d56c2e5ad9bf17aabae
James Ferguson <ncjamesferguson@gmail.com>
2018-02-02 20:22:34 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,12 +1,12 @@
 class Application
-  
+
   @@items = ["Apples","Carrots","Pears"]
   @@cart = []
-  
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-    
+
     if req.path.match(/items/)
       @@items.each do |item|
         resp.write "#{item}\n"
@@ -26,10 +26,10 @@ class Application
     else
       resp.write "Path Not Found"
     end
-    
+
     resp.finish
   end
-  
+
   def handle_search(search_term)
     if @@items.include?(search_term)
       return "#{search_term} is one of our items"
